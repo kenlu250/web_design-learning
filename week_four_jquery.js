c0ac643977b54c22344e682cd4b310f60b28e82c
25040呂紹謙 <43640574+kenlu250@users.noreply.github.com>
2022-03-07 23:50:57 +0800

    Create week_four_jquery.js

---
+++ week_four_jquery.js
@@ -0,0 +1,9 @@
+$(".block1").html("更新公告");
+$(".block2").css("background-color","#f24");
+$(".block2").css("border-radius","100%");
+$(".block3").addClass("bigblock");
+$(".block4").removeClass("bigblock");
+$(".block5").html("<div class=block>商品</div><div class=block>商品</div>");
+$(".block5").prepend("商品列表:");
+$(".block5").append("共有兩項");
+$(".block6").text("<div class=block>商品</div><div class=block>商品</div>");
